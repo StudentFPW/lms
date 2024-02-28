d3853b5f0d31f974f56507be835e4c224854cb92
StudentFPW <jofeleonids00@gmail.com>
2024-02-28 22:01:05 +0200

    28/02/2024

--- lms/lms/settings.py
+++ lms/lms/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(os.getenv("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS").split(" ")
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
